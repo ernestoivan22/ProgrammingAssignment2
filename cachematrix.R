b7a5c01595ab6441f59764e20336cb86284095d8
ernestoivan22 <ernestoivan22@hotmail.com>
2018-02-04 19:37:31 -0600

    added both functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## functions to create a custom matrix object and generate the inverse of said matrix object 
 
-## Write a short comment describing this function
+## this function creates the special matrix object
+## it was based on the example function 'makeVector'
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## this function returns the inverse of the matrix if it was already saved,
+## else it saves it
+## it was based on the example function 'cachemean'
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached inverse of matrix")
+    return(inv)
+  }
+  matrix <- x$get()
+  inv <- solve(matrix, ...)
+  x$setinverse(inv)
+  inv
 }
